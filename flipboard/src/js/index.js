9d1f3174ccaf1dfd32e2354659b523fbd10dc3e2
uiandwe <uiandwe@gmail.com>
2016-07-31 22:18:37 +0900

    index.js  / 드래그 이벤트 추가

--- flipboard/src/js/index.js
+++ flipboard/src/js/index.js
@@ -1,16 +1,50 @@
 $(document).ready(function(){
-    
-   
-    $('.drag').draggable(
-    {
-    	axis: "y",
-        drag: function(){
-            console.log("!")
-            var offset = $(this).offset();
-            console.log('x: ' + offset.left);
-            console.log('y: ' + offset.top);
+
+    var height = $( window ).height();
+    $(".contain-wrapper").css("height", height+"px");
+    $(".drag").css("height", height+"px");
+
+    //$('.drag').draggable(
+    //{
+    //	axis: "y",
+    //    drag: function(){
+    //        var offset = $(this).offset();
+    //        console.log('x: ' + offset.left);
+    //        console.log('y: ' + offset.top);
+    //    }
+    //
+    //});
+
+    var position;
+    var moveY = 0;
+    $('section').mousedown(function(event){
+
+        moveY = 0;
+        position = event;
+    });
+
+    $('section').mouseup(function(event){
+        moveY = position.pageY - event.pageY;
+        var moveId = $(this).attr('id');
+
+        if(moveY > 0){
+            moveId = parseInt(moveId)-1;
+            if(moveId <= 0 ){
+                moveId = 1;
+            }
+
+        }else{
+            moveId = parseInt(moveId)+1;
+            if(moveId >= $(document).find('section').length ){
+                moveId = $(document).find('section').length;
+            }
         }
 
-    });
+        $('.contain-wrapper').animate({
+             scrollTop: $(".contain-wrapper").offset().top + (height*(moveId-1))
+        }, 500);
+
+
+    })
 
 });
